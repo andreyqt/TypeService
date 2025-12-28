fdb0d244e3b835076184946a146008045c7958f9
Andrei Kuzmin <Andreytko@mail.ru>
2025-12-29 02:48:13 +0300

    added open api dependency, set configuration and summary for user controller

---
+++ src/main/java/holymagic/typeservice/config/OpenApiConfig.java
@@ -0,0 +1,43 @@
+package holymagic.typeservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Value("${openapi.dev-url}")
+    private String devUrl;
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Application to interact with MonkeyType API")
+                        .version("1.0")
+                        .description("""
+                        General documentation:
+                        API gives access to:
+                        1) user account data;
+                        2) user test results;
+                        3) Public endpoints...
+                        """
+                        )
+                        .contact(new Contact()
+                                .name("HolyMagic1997")
+                                .url("https://vk.com/holycool"))
+
+                )
+                .servers(List.of(new Server()
+                        .url(devUrl)
+                        .description("Local Server"))
+                );
+    }
+}
--- src/main/java/holymagic/typeservice/controller/UserController.java
+++ src/main/java/holymagic/typeservice/controller/UserController.java
@@ -7,6 +7,7 @@ import holymagic.typeservice.dto.StreakDto;
 import holymagic.typeservice.model.user.Profile;
 import holymagic.typeservice.model.user.Stats;
 import holymagic.typeservice.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,37 +24,44 @@ import java.util.Map;
 public class UserController {
     private final UserService userService;
 
+    @Operation(summary = "Checks to see if a username is available")
     @GetMapping("/checkName/{name}")
     public ResponseEntity<CheckNameDto> checkName(@PathVariable String name) {
         return ResponseEntity.ok(userService.checkName(name));
     }
 
+    @Operation(summary = "Get user's personal bests")
     @GetMapping("/personalBests/{mode}")
     public ResponseEntity<Map<String, List<PersonalBestDto>>> getPersonalBests(@PathVariable String mode) {
         return ResponseEntity.ok(userService.getPersonalBests(mode));
     }
 
+    @Operation(summary = "Gets a user's typing stats data")
     @GetMapping("/personalBests/{mode}/{mode2}")
     public ResponseEntity<List<PersonalBestDto>> getPersonalBests(@PathVariable String mode,
                                                                   @PathVariable String mode2) {
         return ResponseEntity.ok(userService.getPersonalBests(mode, mode2));
     }
 
+    @Operation(summary = "Gets a user's typing stats data")
     @GetMapping("/stats")
     public ResponseEntity<Stats> getStats() {
         return ResponseEntity.ok(userService.getStats());
     }
 
+    @Operation(summary = "Gets a user's profile")
     @GetMapping("/profile/{name}")
     public ResponseEntity<Profile> getProfile(@PathVariable String name) {
         return ResponseEntity.ok(userService.getProfile(name));
     }
 
+    @Operation(summary = "Get test activity for the last up to 372 days for the current user")
     @GetMapping("/activity")
     public ResponseEntity<CurrentTestActivityDto> getCurrentTestActivity() {
         return ResponseEntity.ok(userService.getCurrentTestActivity());
     }
 
+    @Operation(summary = "Get user's streak data")
     @GetMapping("/streak")
     public ResponseEntity<StreakDto> getStreak() {
         return ResponseEntity.ok(userService.getStreak());
