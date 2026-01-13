8f39aa749decea287ab5877cb7d1727ae506f0e2
Andrei Kuzmin <Andreytko@mail.ru>
2026-01-13 23:27:24 +0300

    edit rest config, added exchange service for request specifically

--- src/main/java/holymagic/typeservice/exception/GlobalExceptionHandler.java
+++ src/main/java/holymagic/typeservice/exception/GlobalExceptionHandler.java
@@ -3,8 +3,6 @@ package holymagic.typeservice.exception;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.ws.rs.NotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
--- src/test/java/holymagic/typeservice/service/ExchangeServiceTest.java
+++ src/test/java/holymagic/typeservice/service/ExchangeServiceTest.java
@@ -14,7 +14,6 @@ import org.springframework.web.client.RestClient;
 import java.net.URI;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
